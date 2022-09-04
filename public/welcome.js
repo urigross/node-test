245a88af75231ea7f06b387d46c7c1be363a53a1
urigross <urigross@gmail.com>
2022-09-04 08:51:33 +0300

    added in thegame.html defination of exports

--- public/welcome.js
+++ public/welcome.js
@@ -3,8 +3,8 @@
 
 let redirect_Page = () => {
   let tID = setTimeout(() => {  
-   // window.location.href = "http://localhost:3000/memorygame.html";
-    window.location.href = "https://node-test-git-vercel2-urigross.vercel.app/memorygame.html";
+    window.location.href = "http://localhost:3000/memorygame.html";
+  //  window.location.href = "https://node-test-git-vercel2-urigross.vercel.app/memorygame.html";
     window.clearTimeout(tID); // clear time out.
   }, 9000);
 };
